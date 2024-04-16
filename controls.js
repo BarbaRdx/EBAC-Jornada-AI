ad8d0dcefe12ff4100640d04bf0482058b8dfeea
Lucas Resende <lucasfresende@yahoo.com>
2024-04-16 16:39:07 -0300

    Add files via upload
    Atualização dos arquivos para correto funcionamento após testes do ambiente de console do navegador Chrome.

--- controls.js
+++ controls.js
@@ -1,6 +1,8 @@
 export default {
     jump: new KeyboardEvent ('keydown', {key: 'Space', keyCode: 32}),
-    dispatch(event){
-        document.dispatchEvent(this[event]);
+    crouch: new KeyboardEvent ('keydown', {key: 'ArrowDown', keyCode:40}),
+    
+    dispatch() { 
+        document.dispatchEvent(this.jump); 
     }
 }
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-import utils from "./utils";
-import RNA from "./RNA";
-import controls from "./controls";
+import utils from './utils.js';
+import RNA from './RNA.js';
+import controls from './controls.js';
 
-const SAMPLES = 20;
+const SAMPLES = 2;
 const game = Runner.instance_;
 let dinoList = [];
 let dinoIndex = 0;
@@ -10,82 +10,76 @@ let dinoIndex = 0;
 let bestScore = 0;
 let bestRNA = null;
 
-function fillDinoList() {
-    for (let i=0; i < SAMPLES; i++){
-        dinoList[i] = new RNA(3, [10, 10, 2]);
-        dinoList[i].load(bestRNA);
-        if (i>0){
-            dinoList[i].mutate(0.5);
-        }
-    }
-    console.log('Lista de Dinossauros criada!');
+function fillDinoList () {
+  for (let i=0; i<SAMPLES; i++) {
+    dinoList[i] = new RNA(3, [10, 10, 2]);
+    dinoList[i].load(bestRNA);
+    if (i > 0) dinoList[i].mutate(0.2);
+  }
+  console.log('Dino list created!');
 }
 
 setTimeout(() => {
-    fillDinoList();
-    controls.dispatch('jump');
-}, 1000)
+  fillDinoList ();
+  controls.dispatch('jump')
+}, 1000);
 
 setInterval(() => {
-    if (!game.activated){
-        return;
+  if (!game.activated) return;
+  const dino = dinoList[dinoIndex];
+
+  if (game.crashed) {
+    if (dino.score > bestScore) {
+      bestScore = dino.score;
+      bestRNA = dino.save();
+      console.log('bestScore:', bestScore);
     }
-
-    const dino = dinoList[dinoIndex];
-
-    if(game.crashed){
-        if (dino.score > bestScore) {
-            bestScore = dino.score
-            bestRNA = dino.save()
-            console.log('Melhor Pontuação: ', bestScore);
-        }
-        dinoIndex++
-    
+    dinoIndex++;
 
     if (dinoIndex === SAMPLES) {
-        fillDinoList();
-        dinoIndex = 0;
-        bestScore = 0;
+      fillDinoList();
+      dinoIndex = 0;
+      bestScore = 0;
     }
-    game.restart()
+    game.restart();
   }
 
-  const {tRex, horizon, currentSpeed, distanceRan, dimensions} = game
-  dino.score = distanceRan - 2000
-  
+  const { tRex, horizon, currentSpeed, distanceRan, dimensions } = game;
+  dino.score = distanceRan - 2000;
+
   const player = {
     x: tRex.xPos,
     y: tRex.yPos,
-    speed: currentSpeed
+    speed: currentSpeed,
   };
 
-  const [obstacle] = horizon.obstacle
-  .map((obstacle) => {
-    return {
+  const [obstacle] = horizon.obstacles
+    .map((obstacle) => {
+      return {
         x: obstacle.xPos,
-        y: obstacle.yPos
-    }
-  })
-  .filter((obstacle) => obstacle.x > player.x)
+        y: obstacle.yPos,
+      }
+    })
+    .filter((obstacle) => obstacle.x > player.x)
 
   if (obstacle) {
-    const distance = 1 - (utils.getDistance(player, obstacle) / dimensions.WIDTH)
+    const distance = 1 - (utils.getDistance(player, obstacle) / dimensions.WIDTH);
     const speed = player.speed / 6;
-    const height = Math.tanh(105 - obstacle.y)
+    const height = Math.tanh(105 - obstacle.y);
 
-    const [jump, crouch] = dino.compute([
-        distance,
-        speed,
-        height,
+    const [jump, crounch] = dino.compute([
+      distance,
+      speed,
+      height,
     ]);
 
-    if (jump === crouch) return;
-    if (jump) controls.dispatch('jump') // se for verdadeira o dino pula
-    if (crouch) controls.dispatch('crouch') // se for verdadeira o dino agacha
-  };
+    if (jump === crounch) return;
+    if (jump) controls.dispatch('jump');
+    if (crounch) controls.dispatch('crounch');
+  }
 }, 100);
 
 /* const s = document.createElement('script');
 s.type = 'module';
 s.src = ' http://localhost:5500/script.js';
-document.doctype.appendChild(s); */
+document.body.appendChild(s); */
